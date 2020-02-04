73020fb9d13485e10781ebec401cfe4ddf1fafb5
Juan <jpcerronex@gmail.com>
2020-02-04 16:25:30 -0300

    agregado un visualizador de niveles

--- src/main/java/Ventana.java
+++ src/main/java/Ventana.java
@@ -5,28 +5,69 @@ import engine.helper.Assets;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.nio.Buffer;
 
-public class Ventana extends JFrame {
-    JPanel panel;
+import static java.awt.event.KeyEvent.*;
+
+public class Ventana extends JFrame implements KeyListener {
+    private JPanel panel;
+    private Dimension screenSize;
+    private Image img;
+    private int levelWidth;
+    private int levelHeight;
+    private int xShift;
     Ventana(){
-        panel = new JPanel();
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        super("Level Visualizer");
+        xShift = 0;
+        panel = new Panel();
+        screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         this.setSize(screenSize.width,screenSize.height);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.add(panel);
         Assets.init(getGraphicsConfiguration());
-        //panel.getGraphics().create();
+        addKeyListener(this);
+        setFocusTraversalKeysEnabled(false);
     }
 
-    public void dibujar(int x, int y,Image img){
+    @Override
+    public void keyTyped(KeyEvent e) {
 
-
-        panel.getGraphics().drawImage(img, x, y, 16, 16, null);
     }
 
-    public void dibujarEnemigo(int x, int y,Image img){
-        panel.getGraphics().drawImage(img, x, y, 16, 32, null);
+    @Override
+    public void keyPressed(KeyEvent e) {
+        //keyboard controls
+        switch (e.getKeyCode()) {
+            case VK_RIGHT:
+                if(xShift < levelWidth-screenSize.width)
+                    xShift += 16; break;
+            case VK_LEFT:
+                if(xShift > 0)
+                     xShift -= 16; break;
+        }
+        repaint();
     }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    private class Panel extends JPanel{
+        public void paintComponent(Graphics g){
+            g.drawImage(img, 0 - xShift, screenSize.height/2 -16*16, levelWidth, levelHeight, null);
+        }
+    }
+
+    public void dibujar(Image img, int width, int height){
+        this.img = img;
+        this.levelWidth = width;
+        this.levelHeight = height;
+        repaint();
+    }
+
 }
