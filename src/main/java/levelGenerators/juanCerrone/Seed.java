8cb4a0aa1d262f610f0b1b642230d3118ee01899
Juan <jpcerronex@gmail.com>
2020-03-17 16:22:49 -0300

    agregado Mario a las Seeds

--- src/main/java/levelGenerators/juanCerrone/Seed.java
+++ src/main/java/levelGenerators/juanCerrone/Seed.java
@@ -1,7 +1,7 @@
 package levelGenerators.juanCerrone;
 
 public class Seed {
-    public static final String OVERWORLD = "XX--------------XX--------------XX--------------XX--------------";
-    public static final String UNDERGROUND = "XXSSSSSSSSSSS---XX--------------XX--------------XX--------------";
-    public static final String TREETOPS = "--%-------------||%-------------||%---------------%-------------";
+    public static final String OVERWORLD = "XX--------------XX--------------XXM-------------XX--------------";
+    public static final String UNDERGROUND = "XXSSSSSSSSSSS---XX--------------XXM-------------XX--------------";
+    public static final String TREETOPS = "--%-------------||%-------------||%M--------------%-------------";
 }
