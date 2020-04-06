b39195ffe80806f083e9968e9e2cb37856b54bed
Juan <jpcerronex@gmail.com>
2020-04-06 15:48:46 -0300

    agregado mario al principio de cada nivel original

--- src/main/java/levelGenerators/juanCerrone/Seed.java
+++ src/main/java/levelGenerators/juanCerrone/Seed.java
@@ -2,6 +2,6 @@ package levelGenerators.juanCerrone;
 
 public class Seed {
     public static final String OVERWORLD = "XX--------------XX--------------XXM-------------XX--------------";
-    public static final String UNDERGROUND = "XXSSSSSSSSSSS---XX--------------XXM-------------XX--------------";
+    public static final String UNDERGROUND = "XXSSSSSSSSSSS---XX--------------XX--------------XXM-------------";
     public static final String TREETOPS = "--%-------------||%-------------||%M--------------%-------------";
 }
